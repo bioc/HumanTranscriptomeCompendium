83e450d27fc63f2b8d73e56b3b882e268b5f24db
vjcitn <stvjc@channing.harvard.edu>
2021-10-04 23:31:56 -0400

    drop clean_cache

--- R/htx_clean_cache.R
+++
@@ -1,5 +0,0 @@
-htx_clean_cache = function( cache = BiocFileCache::BiocFileCache() ) {
-  ans = BiocFileCache::bfcquery(cache, "rangedHtxGene")
-  if (length(ans$rid)>0) try( BiocFileCache::bfcremove(cache, ans$rid) )
-}
- 
