77768d30bf1622b575089ef7a67ffbdf4ce07e09
vjcitn <stvjc@channing.harvard.edu>
2023-02-06 12:38:21 -0500

    changes for S4Vectors mods

--- R/htx_load.R
+++ R/htx_load.R
@@ -18,11 +18,15 @@
 ##     ds
 ## }
 
+# this had to change for 3.17 with some changes to S4Vectors
 htx_check_cache = function (cache = BiocFileCache::BiocFileCache(), genesOnly=TRUE) 
 {
 # return 2-vector: c(action, rid if available)
     if (!requireNamespace("BiocFileCache")) stop("install BiocFileCache to use this function")
-    if (genesOnly) qans = BiocFileCache::bfcquery(cache, "rangedHtxGeneSE.rds")
+    if (genesOnly) {
+       if (BiocManager::version() < "3.17") qans = BiocFileCache::bfcquery(cache, "rangedHtxGeneSE.rds")
+       else qans = BiocFileCache::bfcquery(cache, "rangedHtxGeneSE_317.rds")
+       }
     else qans = BiocFileCache::bfcquery(cache, "htxcompSE.rds") # transcript-level
     if (nrow(qans)<1) return(c("install", NA))
     chkupdate = BiocFileCache::bfcneedsupdate(cache, qans$rid[1])
@@ -35,22 +39,30 @@ htx_check_cache = function (cache = BiocFileCache::BiocFileCache(), genesOnly=TR
 #' load a SummarizedExperiment shell for the Human Transcriptome Compendium
 #' @importFrom S4Vectors mcols
 #' @importFrom SummarizedExperiment rowRanges rowData<-
-#' @param remotePath path to an RDS representation of the DelayedArray-based SummarizedExperiment
+#' @param remotePath defaults to NULL, will be removed in future version
 #' @param cache a BiocFileCache instance, defaulting to value of BiocFileCache()
 #' @param genesOnly logical(1) if TRUE return reference to 
 #' SummarizedExperiment with gene-level quantifications; in this 
 #' case the remotePath value is
 #' set to `https://s3.amazonaws.com/bcfound-bigrna/rangedHtxGeneSE.rds`.
+#' As of Bioc 3.17, changes to S4Vectors necessitate different remote images
+#' or redesign of handling HSDS instances.  Thus remotePath parameter is dropped
+#' and version-specific code is added to the loader to select an appropriate path.
 #' @return a RangedSummarizedExperiment instance
 #' @examples
-#' htx_load
+#' if (interactive()) {
+#'   h = htx_load()
+#'   h
+#'   SummarizedExperiment::assay(h)
+#'   }
 #' @export
-htx_load = function (remotePath = "https://biocfound-bigrnatx.s3.us-west-2.amazonaws.com/rangedHtxGeneSE.rds",
+htx_load = function (remotePath = NULL,
     cache = BiocFileCache::BiocFileCache(), genesOnly=TRUE)
 {
     if (!genesOnly) stop("transcript-level quantifications not available currently")
     if (!requireNamespace("BiocFileCache")) stop("install BiocFileCache to use this function")
-    if (genesOnly) remotePath = "https://biocfound-bigrnatx.s3.us-west-2.amazonaws.com/rangedHtxGeneSE.rds"
+    remotePath = "https://bcfound-bigrna.s3.amazonaws.com/rangedHtxGeneSE.rds"
+    if (BiocManager::version() >= "3.17") remotePath = "https://bcfound-bigrna.s3.amazonaws.com/rangedHtxGeneSE_317.rds"
     chkans = htx_check_cache(cache)
     if (chkans[1] == "install") {
         message("adding RDS to local cache, future invocations will use local image")
@@ -61,7 +73,15 @@ htx_load = function (remotePath = "https://biocfound-bigrnatx.s3.us-west-2.amazo
         }
     else if (chkans[1] != "ok") stop("unintended response from cache check")
     path = BiocFileCache::bfcrpath(cache, remotePath)
-    readRDS(path)
+    ans = readRDS(path)
+#    if (BiocManager::version() >= "3.17") { # new reference needed
+#           harr = rhdf5client::HSDSArray(rhdf5client::URL_hsds(), 
+#              "hsds", "/shared/bioconductor/htxcomp_genes.h5", "/counts")
+#           requireNamespace("S4Vectors")
+#           sl = new("SimpleAssays", data=SimpleList(counts_lstpm=harr)) 
+#           slot(ans, "assays") = sl
+#           }
+     ans
 }
 
 
